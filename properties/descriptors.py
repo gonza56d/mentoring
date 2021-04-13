5d8c1f3305603b35fcba1b57e84c934c2eeed577
Gonza Garcia <gonzalo.garcia01@globant.com>
2021-04-13 16:20:08 -0300

    tuesday

--- properties/descriptors.py
+++ properties/descriptors.py
@@ -1,41 +1,47 @@
-
-# READ ONLY DESCRIPTOR:
-
-class VerboseAttribute:
-
-    def __get__(self, obj, type=None) -> object:
-        print("accessing the attribute to get the value")
-        return 42
-
-    def __set__(self, obj, value) -> None:
-        print("accessing the attribute to set the value")
-        raise AttributeError("Cannot change the value")
+"""https://www.youtube.com/watch?v=ovsvGtWD90Y&t=258s"""
 
 
-class Foo:
-    attribute1 = VerboseAttribute()
+class PersonName:
+
+    def __init__(self, initial_value: str = ''):
+        self.val = initial_value
+
+    def __get__(self, instance, owner):
+        return self.val
+
+    def __set__(self, instance, value):
+        self.val = value.upper()
+
+    def __str__(self):
+        return self.val
 
 
-my_foo_object = Foo()
-x = my_foo_object.attribute1
-print(x)
-# my_foo_object.attribute1 = 3  # AttributeError (recommended way to implement read-only descriptors)
+class PersonAge:
+
+    def __init__(self, initial_value: int = 0):
+        self.val = initial_value
+
+    def __get__(self, instance, owner):
+        return self.val
+
+    def __set__(self, instance, value):
+        if value > 120:
+            raise ValueError("Person max. age possible is 120")
+        self.val = value
+
+    def __str__(self):
+        return str(self.val)
 
 
 class Person:
 
-    def __init__(self, name: str):
-        self.name = name
+    def __init__(self, name: str = '', age: int = 0):
+        self.name = PersonName(name)
+        self.age = PersonAge(age)
 
-    @property
-    def name(self) -> str:
-        return self.name
-
-    @name.setter
-    def name(self, value: str) -> None:
-        raise AttributeError("Name cannot be changed")
+    def __str__(self):
+        return f'{self.name} is {self.age} years old.'
 
 
-person = Person(name='Gonza')
-print(person.name)
-person.name = 'Killian'
+gonza = Person(name='gonza', age=24)
+print(gonza)
